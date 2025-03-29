6fd3a3f0ed3502cad7e72c5df6e44bb79ae74d5c
Switi <45126465+pswiti@users.noreply.github.com>
2025-03-28 18:28:32 -0700

    Update main.tf

--- terraform/serverless/main.tf
+++ terraform/serverless/main.tf
@@ -11,39 +11,35 @@ resource "aws_vpc" "main" {
 
 # Public Subnet 1
 resource "aws_subnet" "public_subnet_1" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-west-2a"
-  map_public_ip_on_launch = true
-  subnet_type = "public"
+  vpc_id                    = aws_vpc.main.id
+  cidr_block                = "10.0.1.0/24"
+  map_public_ip_on_launch   = true
+  subnet_type               = "public"
 }
 
 # Public Subnet 2
 resource "aws_subnet" "public_subnet_2" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-west-2b"
-  map_public_ip_on_launch = true
-  subnet_type = "public"
+  vpc_id                    = aws_vpc.main.id
+  cidr_block                = "10.0.2.0/24"
+  map_public_ip_on_launch   = true
+  subnet_type               = "public"
 }
 
 # Private Subnet 1
 resource "aws_subnet" "private_subnet_1" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.3.0/24"
-  availability_zone = "us-west-2a"
-  subnet_type = "private"
+  vpc_id                    = aws_vpc.main.id
+  cidr_block                = "10.0.3.0/24"
+  subnet_type               = "private"
 }
 
 # Private Subnet 2
 resource "aws_subnet" "private_subnet_2" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.4.0/24"
-  availability_zone = "us-west-2b"
-  subnet_type = "private"
+  vpc_id                    = aws_vpc.main.id
+  cidr_block                = "10.0.4.0/24"
+  subnet_type               = "private"
 }
 
-# Create an IAM Role for Lambda
+# IAM Role for Lambda
 resource "aws_iam_role" "lambda_role" {
   name               = "lambda-execution-role"
   assume_role_policy = jsonencode({
