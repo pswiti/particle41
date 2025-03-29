57d8a88ca7f6a84187ba94ce48c7621ab8290d2d
Switi <45126465+pswiti@users.noreply.github.com>
2025-03-29 01:04:33 -0700

    Create vpc.tf

---
+++ terraform/modules/vpc/vpc.tf
@@ -0,0 +1,42 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr_block
+  enable_dns_support = true
+  enable_dns_hostnames = true
+}
+
+resource "aws_subnet" "public" {
+  count = 2
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.public_subnet_cidr_blocks[count.index]
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Public Subnet ${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count = 2
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.private_subnet_cidr_blocks[count.index]
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+  tags = {
+    Name = "Private Subnet ${count.index + 1}"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnets" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnets" {
+  value = aws_subnet.private[*].id
+}
