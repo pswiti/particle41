d0f3b4d063313cd0a65127c1890b2c08579f65a1
Switi <45126465+pswiti@users.noreply.github.com>
2025-03-29 01:03:10 -0700

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+variable "vpc_cidr_block" {
+  type        = string
+  description = "The CIDR block for the VPC."
+}
+
+variable "public_subnet_cidr_blocks" {
+  type        = list(string)
+  description = "CIDR blocks for public subnets."
+}
+
+variable "private_subnet_cidr_blocks" {
+  type        = list(string)
+  description = "CIDR blocks for private subnets."
+}
+
+variable "container_image" {
+  type        = string
+  description = "The container image for ECS."
+}
+
+variable "lambda_zip_file" {
+  type        = string
+  description = "The Lambda zip file for deployment."
+}
