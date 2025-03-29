4f642f44eb42eb882256c39ec4342691954d37bf
Switi <45126465+pswiti@users.noreply.github.com>
2025-03-29 15:36:03 -0700

    Update variables.tf

--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -1,3 +1,14 @@
+# Number of public subnets to create
+variable "public_subnet_count" {
+  description = "The number of public subnets to create"
+  type        = number
+  }
+
+# Number of private subnets to create
+variable "private_subnet_count" {
+  description = "The number of private subnets to create"
+  type        = number
+  }
 variable "vpc_cidr_block" {
   type        = string
   description = "The CIDR block for the VPC."
