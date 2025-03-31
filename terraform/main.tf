cea494c9c74581c49fdb1efa065c7bfd60221c18
Switi <45126465+pswiti@users.noreply.github.com>
2025-03-31 16:59:12 -0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,11 +4,11 @@ provider "aws" {
 
 module "vpc" {
   source = "./modules/vpc"
-  vpc_cidr_block        = "10.0.0.0/16"
-  public_subnet_cidr_blocks = ["10.0.1.0/24", "10.0.2.0/24"]
+  vpc_cidr_block             = "10.0.0.0/16"
+  public_subnet_cidr_blocks  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnet_cidr_blocks = ["10.0.3.0/24", "10.0.4.0/24"]
-  public_subnet_count  = 2 
-  private_subnet_count = 2 
+  public_subnet_count        = 2 
+  private_subnet_count       = 2 
 }
 
 module "ecs" {
